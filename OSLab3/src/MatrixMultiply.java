aef0851db5d6f1c5541a6050f44edb7271c6a6a4
Ayush Kumar <ayushkr19@gmail.com>
2015-08-28 12:32:54 +0530

    Documentation

--- OSLab3/src/MatrixMultiply.java
+++ OSLab3/src/MatrixMultiply.java
@@ -3,20 +3,39 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Multiplies two matrices using multithreading
+ * @author ayush
+ *
+ */
 public class MatrixMultiply {
 
+	/**
+	 * The first matrix to be multiplied
+	 */
 	int arr[][];
+	
+	/**
+	 * The second matrix to be multiplied
+	 */
 	int arr2[][];
+	
+	/**
+	 * The dimensions of 1st matrix : m * n
+	 * The dimensions of 2nd matrix : n * l
+	 */
 	int m,n,l;
+	
+	/**
+	 * The resultant matrix. Dimensions : m * l
+	 */
 	int result[][];
 	
 	
 	public MatrixMultiply() throws FileNotFoundException {
 		
-//		System.out.println("Enter the number of threads to be created: ");
 		//Scanner sc = new Scanner(System.in);
 		Scanner sc = new Scanner(new FileReader("input.txt"));
-//		k = sc.nextInt();
 		
 		System.out.println("Enter 1st matrix's m (rows): ");
 		m = sc.nextInt();
@@ -26,6 +45,7 @@ public class MatrixMultiply {
 		
 		arr = new int[m][n];
 		
+		// Get the first matrix
 		for(int i=0; i< m; i++){
 			for(int j=0; j<n; j++){
 				arr[i][j] = sc.nextInt();
@@ -37,15 +57,20 @@ public class MatrixMultiply {
 		
 		arr2 = new int[n][l];
 		
+		//Get the second matrix
 		for(int i=0; i< n; i++){
 			for(int j=0; j<l; j++){
 				arr2[i][j] = sc.nextInt();
 			}
 		}
 		
+		//Initialize result matrix with proper dimensions
 		result = new int[m][l];
 	}
 	
+	/**
+	 * Print the matrices entered
+	 */
 	public void print(){
 		
 		System.out.println("First matrix entered : ");
@@ -66,6 +91,9 @@ public class MatrixMultiply {
 		
 	}
 	
+	/**
+	 * Print resultant matrix after multiplication
+	 */
 	public void printResult(){
 		System.out.println("\n\nMultiplication result matrix : ");
 
@@ -77,16 +105,32 @@ public class MatrixMultiply {
 		}
 	}
 	
+	/**
+	 * Calculate the matrix multiplication using multiple threads
+	 * A thread is created for each row of the 1st matrix.
+	 * @throws InterruptedException
+	 */
 	public void calculate() throws InterruptedException{
+		
+		
+		// Hold all threads in one structure
 		ArrayList<Thread> threads = new ArrayList<Thread>();
+		
 		for(int i=0; i<m; i++){
+			
+			// Create thread
 			MultiplierThread mul = new MultiplierThread(i, arr, arr2, m, n, l, result[i]);
 			Thread t = new Thread(mul);
+			
+			// Add thread to ArrayList
 			threads.add(t);
+			
+			// Start the thread
 			t.start();
 			Util.println(t.getName() + " is starting!");
 		}
 		
+		// Wait for all threads to finish
 		for(Thread t: threads){
 			t.join();
 		}
--- OSLab3/src/MultiplierThread.java
+++ OSLab3/src/MultiplierThread.java
@@ -1,4 +1,9 @@
-
+/**
+ * Thread to multiply one row of a matrix with all columns of another matrix
+ * and store the result in a result matrix (1D array)
+ * @author ayush
+ *
+ */
 public class MultiplierThread implements Runnable {
 
 	public int rowNumber;
@@ -7,6 +12,16 @@ public class MultiplierThread implements Runnable {
 	int m,n,l;
 	int result[];
 
+	/**
+	 * Constructor
+	 * @param rowNumber Operate on this row of the 1st matrix
+	 * @param arr First matrix
+	 * @param arr2 Second matrix
+	 * @param m 1st matrix's rows
+	 * @param n 1st matrix's columns
+	 * @param l 2nd matrix's columns
+	 * @param result rowNumber'th row of result matrix
+	 */
 	public MultiplierThread(int rowNumber, int[][] arr, int[][] arr2, int m, int n, int l, int result[]) {
 		super();
 		this.rowNumber = rowNumber;
@@ -23,10 +38,13 @@ public class MultiplierThread implements Runnable {
 	@Override
 	public void run() {
 		
+		// Initialize matrix to zero
 		for(int i=0; i<l; i++){
 			result[i] = 0;
 		}
 
+		// Perform actual multiplication of row of 1st matrix with columns of 2nd matrix
+		// Store results in corresponding row of result matrix
 		for(int i=0; i<l; i++){
 			for(int j=0; j<n; j++){
 				Util.println("Thread " + rowNumber + ", Loop : " + i + " " + j);
@@ -36,13 +54,15 @@ public class MultiplierThread implements Runnable {
 				Util.println("Thread " + rowNumber + ", " + arr2[j][i]);
 
 				Util.println("Thread " + rowNumber + ", " + arr[rowNumber][j]*arr2[j][i]);
+				
 				result[i] += arr[rowNumber][j]*arr2[j][i];
 
 				Util.println("Thread " + rowNumber + ", Final result : " + result[i]);
 			}
 
 		}
-
+		
+		// Display results (for debugging only)
 		for(int i=0; i<l; i++){
 			Util.println("Thread " + rowNumber);
 			Util.print(result[i] + " ");
